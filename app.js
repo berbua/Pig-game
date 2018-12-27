b03e07c2a6e6b18d3598cec03e879914f26d1f41
Katarzyna Berbeka <katarzyna.berbeka@creativestyle.pl>
2018-12-27 11:46:40 +0100

    test

--- app.js
+++ app.js
@@ -6,40 +6,39 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
 const scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
 
-document.querySelector(".dice").style.display = "none";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector(".btn-roll").addEventListener("click", function() {
+document.querySelector('.btn-roll').addEventListener('click', function() {
   let dice = Math.floor(Math.random() * 6) + 1;
 
-  const diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + dice + ".png";
+  const diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
 
   if (dice !== 1) {
     roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     nextPlayer();
   }
 });
 
-document.querySelector(".btn-hold").addEventListener("click", function() {
+document.querySelector('.btn-hold').addEventListener('click', function() {
   // add current score to global score
   scores[activePlayer] += roundScore;
 
   //update the UI
-  document.getElementById("score-" + activePlayer).textContent =
+  document.getElementById('score-' + activePlayer).textContent =
     scores[activePlayer];
 
   //check if player won the game
@@ -51,9 +50,9 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
 function nextPlayer() {
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   roundscore = 0;
-  document.getElementById("current-0").textContent = "0";
-  document.getElementById("current-1").textContent = "0";
-  document.querySelector(".player-0-panel").classList.toggle("active");
-  document.querySelector(".player-1-panel").classList.toggle("active");
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
 }
